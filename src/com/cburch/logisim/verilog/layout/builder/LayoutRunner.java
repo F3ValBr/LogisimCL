74c4d7804bd4df6e9742209e047db15ada163de5
Felipe Valdebenito Bravo <felipe.vb1806@gmail.com>
2025-11-17 01:19:36 -0300

    fix(layoutRunner): fixed metadata provider

--- src/com/cburch/logisim/verilog/layout/builder/LayoutRunner.java
+++ src/com/cburch/logisim/verilog/layout/builder/LayoutRunner.java
@@ -1,6 +1,8 @@
 package com.cburch.logisim.verilog.layout.builder;
 
+import org.eclipse.elk.alg.layered.options.LayeredMetaDataProvider;
 import org.eclipse.elk.core.RecursiveGraphLayoutEngine;
+import org.eclipse.elk.core.data.LayoutMetaDataService;
 import org.eclipse.elk.core.util.NullElkProgressMonitor;
 import org.eclipse.elk.graph.ElkNode;
 
@@ -8,7 +10,16 @@ import org.eclipse.elk.graph.ElkNode;
  * Ejecuta el layout en un grafo ELK.
  */
 public final class LayoutRunner {
-    private static final RecursiveGraphLayoutEngine ENGINE = new RecursiveGraphLayoutEngine();
+    private static final RecursiveGraphLayoutEngine ENGINE;
+
+    static {
+        // Registro explícito del algoritmo "layered"
+        LayoutMetaDataService
+                .getInstance()
+                .registerLayoutMetaDataProviders(new LayeredMetaDataProvider());
+
+        ENGINE = new RecursiveGraphLayoutEngine();
+    }
 
     /**
      * Ejecuta el layout en el grafo ELK dado.
